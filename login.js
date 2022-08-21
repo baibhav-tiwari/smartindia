ec42535525fe6f5f55ba2f65f2dd85d1d52c6b1b
baibhav tiwari <baibhavtiwari37@gmail.com>
2022-08-22 04:28:31 +0530

    master

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if (username == "admin@org" && password == "@org") {
-        alert(window.open("organisation.html"));
+        alert(window.open("org.html"));
         return false;
     }
     else if (username == "admin@hei" && password == "@hei") {
