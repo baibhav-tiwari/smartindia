940a6bf6b21c75ab3fbc1ab2721aa621df1c4d3b
baibhav tiwari <baibhavtiwari37@gmail.com>
2022-08-26 15:06:26 +0530

    master

--- login.js
+++ login.js
@@ -2,11 +2,11 @@ function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if (username == "admin@org" && password == "@org") {
-        alert(window.open("https://baibhav--tiwari-github-io.translate.goog/smartindia/org.html?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=en-US&_x_tr_pto=wapp"));
+        alert(window.open("org.html"));
         return false;
     }
     else if (username == "admin@hei" && password == "@hei") {
-        alert(window.open("https://baibhav--tiwari-github-io.translate.goog/smartindia/hei.html?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=en-US&_x_tr_pto=wapp"));
+        alert(window.open("hei.html"));
         return false;
 
     }
